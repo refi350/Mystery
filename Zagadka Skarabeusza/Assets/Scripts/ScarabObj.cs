3d243f37fa0be6d2c1ed472627276b1bd97cbbda
refi350 <refi350@wp.pl>
2020-03-04 21:10:05 +0100

    Added Repository Files

---
+++ Zagadka Skarabeusza/Assets/Scripts/ScarabObj.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScarabObj : MonoBehaviour
+{
+    [SerializeField]private Sprite activeScarabSprite;
+    [SerializeField]private Sprite inactiveScarabSprite;
+    [SerializeField]private Sprite goldenScarabSprite;
+
+    private bool isActivated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetScarabPos()
+    {
+        return transform.position;
+    }
+
+    // targetSprite - 0(inactiveScarabSprite), 1(activeScarabSprite), 2(goldenScarabSprite)
+    private void ChangeScrabSprite(int targetSprite)
+    {
+
+    }
+}
